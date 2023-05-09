f8eb30a7ef35e424ba944321181440568ca206d0
Denis Pochinskiy <ircxenon1@gmail.com>
2023-05-09 09:03:16 +0500

    0705 upd

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,22 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import { BrowserRouter, Route } from "react-router-dom";
 
 
-
+  
 
 const App = (props: any) => {
   return (
     <BrowserRouter>
-    <div className="wrapper">
-      <Header />
-      <Navigation />
-      <div className="app-wrapper-content">
-        <Route exact path="/profile" component={Main}/>
-        <Route exact path="/dialogs" component={Dialogs} />
-        <Route exact path="/news" component={Dialogs} />
+      <div className="wrapper">
+        <Header />
+        <Navigation navigationData={props.data.navigationData}/>
+        <div className="app-wrapper-content">
+          <Route exact path="/profile" render={() => <Main profileData={props.data.profileData}/>} />
+          <Route exact path="/dialogs" render={() => <Dialogs dialogsData={props.data.dialogsData}/>} />
+          {/* <Route exact path="/news" component={Dialogs} />
         <Route exact path="/music" component={Dialogs} />
-        <Route exact path="/settings" component={Dialogs} />
+        <Route exact path="/settings" component={Dialogs} /> */}
+        </div>
       </div>
-    </div>
     </BrowserRouter>
   );
 };
--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -4,13 +4,24 @@ import s from "../Dialogs.module.css";
 
 type MessagePropType = {
   messageContent: string;
-}
-
+};
 
 // ~~~~~~~~~~~~~~~~~~~ JSX Component ~~~~~~~~~~~~~~~~~~~~ //
 
-  const Message = (props: MessagePropType) => (
+const Message = (props: MessagePropType) => (
+  <div className={s.message__container}>
+    <img
+      className={s.message__status__circle}
+      src="https://photogora.ru/img/product/big/6032/14780170120.jpg"
+      alt="status__message"
+    />
     <div className={s.dialogs__message}>{props.messageContent}</div>
-  );
+    <img
+      className={s.message__status__circle}
+      src="https://www.ltmaster.ru/image/cache/catalog/image/cache/catalog/LDSP/belyi-sveza-800x800.webp"
+      alt="status__message"
+    />
+  </div>
+);
 
-  export default Message;
+export default Message;
