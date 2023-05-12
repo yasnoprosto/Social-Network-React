744ee1616344d927acf3a70efa1ef699e29f788f
Denis Pochinskiy <ircxenon1@gmail.com>
2023-05-12 07:50:36 +0500

    1105 upd

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,17 @@ import Profile from "./components/Profile/Profile";
 import Navigation from "./components/Navigation/Navigation";
 import Dialogs from "./components/Dialogs/Dialogs";
 import { BrowserRouter, Route } from "react-router-dom";
-
-
   
 
 const App = (props: any) => {
+  // debugger
   return (
     <BrowserRouter>
       <div className="wrapper">
         <Header />
         <Navigation navigationData={props.data.navigationData}/>
         <div className="app-wrapper-content">
-          <Route exact path="/profile" render={() => <Profile profileData={props.data.profileData}/>} />
+          <Route exact path="/profile" render={() => <Profile profileData={props.data.profileData} addPostData={props.addPostData} updatePostText={props.updatePostText}/>} />
           <Route exact path="/dialogs" render={() => <Dialogs dialogsData={props.data.dialogsData}/>} />
           {/* <Route exact path="/news" component={Dialogs} />
         <Route exact path="/music" component={Dialogs} />
